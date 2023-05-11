592cf9bc6da12bfd774946a83bd44d69461b7712
josias.ruegg <josias.ruegg@BEELINKRYZEN>
2023-05-11 17:09:37 -0500

    Add project files.

---
+++ Lab2/CuentaYape.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab2
+{
+    public class CuentaYape
+    {
+        float saldo;
+        float userId;
+
+        float consultaSaldo()
+        {
+            return 0;
+        }
+
+        void recargaCuenta(float monto)
+        {
+            return;
+        }
+
+        void pagar(CuentaYape cuentaAbono)
+        {
+            return;
+        }
+    }
+}
---
+++ SpecFlowProject/Features/G1.feature
@@ -0,0 +1,9 @@
+﻿Feature: G1
+
+Gherkin 1
+
+@tag1
+Scenario: [scenario name]
+	Given [context]
+	When [action]
+	Then [outcome]
